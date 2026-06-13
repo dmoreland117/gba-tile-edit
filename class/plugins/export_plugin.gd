f59b837f44a11c76ae5d2b6f7c138b307dff3602
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2026-06-13 13:50:41 -0400

    Update export_plugin.gd: changed type args to category

--- class/plugins/export_plugin.gd
+++ class/plugins/export_plugin.gd
@@ -7,15 +7,15 @@ signal export_params_update_requested()
 @abstract
 func get_exporter_name() -> String
 @abstract
-func get_supported_types() -> Array[String]
+func get_supported_categories() -> Array[String]
 @abstract
 func get_export_file_filters() -> Array[String]
 @abstract
-func get_export_params(type:String) -> Dictionary
+func get_export_params(category:String) -> Dictionary
 @abstract
-func export(path:String, type:String, params) -> bool
+func export(path:String, category:String, params) -> bool
 @abstract
-func get_preview(type:String, params) -> Control
+func get_preview(category:String, params) -> Control
 
 func request_update_export_params() -> void:
 	export_params_update_requested.emit()
