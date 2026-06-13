eb92946f21c2b655051def3450a0320dc6d40a62
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2026-06-13 14:28:11 -0400

    moved menubar test code to base plugin

--- main.gd
+++ main.gd
@@ -1,75 +1,10 @@
 extends Control
 
-# start app > 
-
 
 func _ready() -> void:
-	Project.create_initail_project()
-	
 	Settings.load()
 	
-	var file_popup_menu = PopupMenu.new()
-	file_popup_menu.name = 'File'
-	file_popup_menu.add_item('Open', -1, KEY_MASK_CTRL | KEY_O)
-	file_popup_menu.add_item('Save', -1, KEY_MASK_CTRL | KEY_S)
-	file_popup_menu.add_item('Save As', -1, KEY_MASK_CTRL | KEY_MASK_ALT | KEY_S)
-	
-	file_popup_menu.id_pressed.connect(
-		func(id):
-			match id:
-				0:
-					CommandPalette.call_command('load')
-				1:
-					CommandPalette.call_command('save')
-				2:
-					CommandPalette.call_command('saveas')
-	)
-	
-	var edit_popup_menu = PopupMenu.new()
-	edit_popup_menu.name = 'Edit'
-	edit_popup_menu.add_item('Create Palette', -1, KEY_MASK_CTRL | KEY_MASK_SHIFT | KEY_P)
-	edit_popup_menu.add_item('Create Tileset', -1, KEY_MASK_CTRL | KEY_MASK_SHIFT | KEY_T)
-	edit_popup_menu.add_item('Settings', -1, KEY_MASK_CTRL | KEY_COMMA)
-	
-	edit_popup_menu.id_pressed.connect(
-		func(id):
-			match id:
-				0:
-					CommandPalette.call_command('addpalette', 'test', GBPaletteData.PALETTE_MODE_FIXED)
-				1:
-					Popups.show_create_tileset_window()
-				2:
-					Popups.show_settings_window()
-	)
-	
-	Ui.get_menu_bar().add_child(file_popup_menu)
-	Ui.get_menu_bar().add_child(edit_popup_menu)
-	
-	CommandPalette.register_command(
-		'save',
-		[],
-		save_proj
-	)
-	CommandPalette.register_command(
-		'saveas',
-		[],
-		save_proj_as
-	)
-	CommandPalette.register_command(
-		'load',
-		[],
-		load_proj
-	)
-	
-	var save_btn = Button.new()
-	save_btn.text = 'Save Project'
-	save_btn.theme_type_variation = 'primary_btn'
-	save_btn.pressed.connect(
-		func():
-			CommandPalette.call_command('save')
-	)
-	
-	Ui.get_container(Ui.TOP_RIGHT_CONTIANER).add_child(save_btn)
+	Project.create_initail_project()
 	
 	PluginManager.scan_dir_paths()
 	
@@ -78,33 +13,3 @@ func _ready() -> void:
 	var w:Window = get_window()
 	w.content_scale_factor = 1
 	pass
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("show_cmd_palette"):
-		Popups.show_command_palette()
-	if Input.is_action_just_pressed("add_palette_color"):
-		Project.palette.add_color()
-
-func save_proj():
-	var path
-	if Project.last_save_path.is_empty():
-		path = await Popups.show_save_file_popup(['*.gbproj'])
-	else:
-		path = Project.last_save_path
-	
-	#Project.save(path)
-	ProjectSaver.save_project_file(path)
-	
-	return true
-
-func save_proj_as():
-	var path = await Popups.show_save_file_popup(['*.gbproj'])
-	Project.save(path)
-	
-	return true
-
-func load_proj():
-	var path = await Popups.show_open_file_popup(['*.gbproj'])
-	ProjectSaver.load_project_file(path)
-	
-	return true
--- plugins/base/base_plugin.gd
+++ plugins/base/base_plugin.gd
@@ -9,6 +9,62 @@ var tile_editor_instance:TileEditor
 
 
 func _enter_tree() -> void:
+	register_exporter_plugin(GBACHeaderExporter.new())
+	register_exporter_plugin(CX16.new())
+	
+	var file_popup_menu = PopupMenu.new()
+	file_popup_menu.name = 'File'
+	file_popup_menu.add_item('Open', -1, KEY_MASK_CTRL | KEY_O)
+	file_popup_menu.add_item('Save', -1, KEY_MASK_CTRL | KEY_S)
+	file_popup_menu.add_item('Save As', -1, KEY_MASK_CTRL | KEY_MASK_ALT | KEY_S)
+	
+	file_popup_menu.id_pressed.connect(
+		func(id):
+			match id:
+				0:
+					CommandPalette.call_command('load')
+				1:
+					CommandPalette.call_command('save')
+				2:
+					CommandPalette.call_command('saveas')
+	)
+	
+	var edit_popup_menu = PopupMenu.new()
+	edit_popup_menu.name = 'Edit'
+	edit_popup_menu.add_item('Create Palette', -1, KEY_MASK_CTRL | KEY_MASK_SHIFT | KEY_P)
+	edit_popup_menu.add_item('Create Tileset', -1, KEY_MASK_CTRL | KEY_MASK_SHIFT | KEY_T)
+	edit_popup_menu.add_item('Settings', -1, KEY_MASK_CTRL | KEY_COMMA)
+	
+	edit_popup_menu.id_pressed.connect(
+		func(id):
+			match id:
+				0:
+					CommandPalette.call_command('addpalette', 'test', GBPaletteData.PALETTE_MODE_FIXED)
+				1:
+					Popups.show_create_tileset_window()
+				2:
+					Popups.show_settings_window()
+	)
+	
+	Ui.get_menu_bar().add_child(file_popup_menu)
+	Ui.get_menu_bar().add_child(edit_popup_menu)
+	
+	CommandPalette.register_command(
+		'save',
+		[],
+		save_proj
+	)
+	CommandPalette.register_command(
+		'saveas',
+		[],
+		save_proj_as
+	)
+	CommandPalette.register_command(
+		'load',
+		[],
+		load_proj
+	)
+	
 	#init palette editor
 	palette_editor_instance = PALETTE_EDITOR.instantiate()
 	Ui.get_container(Ui.LEFT_CONTAINER).add_child(palette_editor_instance)
@@ -27,3 +83,27 @@ func add_palette(pal_name:String):
 	var pal = RGBPaletteData.new()
 	pal.set_palette_name(pal_name)
 	Project.palettes.add_palette(pal)
+
+func save_proj():
+	var path
+	if Project.last_save_path.is_empty():
+		path = await Popups.show_save_file_popup(['*.gbproj'])
+	else:
+		path = Project.last_save_path
+	
+	#Project.save(path)
+	ProjectSaver.save_project_file(path)
+	
+	return true
+
+func save_proj_as():
+	var path = await Popups.show_save_file_popup(['*.gbproj'])
+	Project.save(path)
+	
+	return true
+
+func load_proj():
+	var path = await Popups.show_open_file_popup(['*.gbproj'])
+	ProjectSaver.load_project_file(path)
+	
+	return true
